0d36e31a5fcca72d6ba1bf4e335234b320b6a4d6
Somesh Chavhan <someshchavhan904@gmail.com>
2024-01-06 20:21:14 +0530

    navbar added

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main>
+    <main className="max-w-[1600px] mx-auto">
 
     </main>
   )
--- components/AppBar.jsx
+++ components/AppBar.jsx
@@ -1,10 +1,15 @@
+import Image from "next/image"
 
 
 const AppBar = () => {
     return (
-        <header>
-            <nav>
-
+        <header className="border-b-2 border-gray-400 bg-[#1D267D]">
+            <nav className="w-11/12 lg:10/12 mx-auto py-2 flex-center">
+                <Image
+                    src={"/assets/todo-logo.png"}
+                    width={110}
+                    height={50}
+                />
             </nav>
         </header>
     )
