69c219c5a26115cec8a9e9f18ed1ab7fa5828748
Somesh Chavhan <someshchavhan904@gmail.com>
2024-01-06 20:45:38 +0530

    form compoent completed

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,10 +1,26 @@
 'use client'
 import { useState } from 'react'
 
+const initialData = {
+    title: "",
+    desc: '',
+}
+
 const Form = () => {
+    const [formData, setFormData] = useState(initialData)
+
+    const handleChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(formData)
+    }
+
     return (
         <div className="lg:w-1/2 md:w-8/12 w-full">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="flex flex-col gap-y-3 ">
                     <label
                         htmlFor="title"
@@ -13,6 +29,8 @@ const Form = () => {
                     <input
                         type="text"
                         name="title"
+                        value={formData.title}
+                        onChange={handleChange}
                         className="border-none bg-white text-black p-2 focus:outline-1 outline-[#1D267D] rounded-lg text-lg"
                     />
                 </div>
@@ -24,8 +42,18 @@ const Form = () => {
                     <input
                         type="text"
                         name="desc"
+                        value={formData.desc}
+                        onChange={handleChange}
                         className="border-none bg-white text-black p-2 focus:outline-1 outline-[#1D267D] rounded-lg text-lg"
                     />
+                </div>
+                <div className='flex-center mt-5'>
+                    <button
+                        type="submit"
+                        className='bg-[#5C469C] px-4 text-xl py-2 rounded-full'
+                    >
+                        Add todo
+                    </button>
                 </div>
             </form>
         </div>
